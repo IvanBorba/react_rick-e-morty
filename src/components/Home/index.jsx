d2645d9cd9a97c9544d805fe98ade5f43e2260df
Ivan Borba <mrborbaivan@gmail.com>
2022-06-01 23:04:15 -0300

    feature: Aula 8 - finalizada

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,39 @@
-const Home = (props) => {
+import Card from "../Card";
+import { useState, useEffect } from "react";
+import "./style.css";
+
+const Home = () => {
+  const [characters, setCharacters] = useState([]);
+  const [url, setUrl] = useState("https://rickandmortyapi.com/api/character");
+
+  const getCharacters = async () => {
+    const response = await fetch(url);
+    const list = await response.json();
+
+    setCharacters([...characters, ...list.results]);
+    setUrl(list.info.next);
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  useEffect(() => {
+    if (!url) {
+      getCharacters();
+    }
+  }, [url]);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <h1>Lista de Personagens</h1>
+      <div className="cardsList-container">
+        {characters.map((element) => {
+          return (
+            <Card key={element.id} name={element.name} image={element.image} />
+          );
+        })}
+      </div>
     </div>
   );
 };
